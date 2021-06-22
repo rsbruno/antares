3c7b9d9ed35bec00e8c72bfd5ca47ebe57249560
rsbruno <bruno.roberto@sou.unifal-mg.edu.br>
2021-06-22 08:27:40 -0300

    feat(fonts)fonts google Rajdhani and Inter added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import {useFonts} from 'expo-font'
+import AppLoading from 'expo-app-loading';
 
-import { Container,Title } from './src/global/styles/common';
+import { Inter_500Medium, Inter_700Bold } from '@expo-google-fonts/inter';
+import { Rajdhani_400Regular, Rajdhani_500Medium } from '@expo-google-fonts/rajdhani';
+
 import {SignIn} from './src/screens/SignIn'
 
-
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Inter_500Medium,Inter_700Bold,
+    Rajdhani_400Regular, Rajdhani_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } 
+
   return (
     <SignIn />
   );
---
+++ src/global/constants/fonts.ts
@@ -0,0 +1,4 @@
+export const title700 = "Rajdhani_700Bold";
+export const title500 = "Rajdhani_500Medium";
+export const text400 = "Inter_400Regular";
+export const text500 = "Inter_500Medium";
